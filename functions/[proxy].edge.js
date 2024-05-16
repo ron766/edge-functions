51041f70bb2e5674cc7e3fd3f37833c23f651f36
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-05-16 18:45:27 +0530

    update url

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -3,7 +3,8 @@ export default async function handler(req, context) {
   const route = parsedUrl.pathname;
   const envVariable = context.env.TEST_KEY;
 
-  if (route === '/users-with-edge-fetch') {
+  if (route === '/test') {
+    //const res = await fetch(`https://random-data-api.com/api/v2/appliances`);
     const res = await fetch(`https://demo-site-edge.devcontentstackapps.com/users`);
     let response = await res.json();
     response = {
