25becee15a8ee3446d029d06ca447bcbc7c9f67f
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-05-16 20:01:38 +0530

    fix

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -3,21 +3,7 @@ export default async function handler(req, context) {
   const route = parsedUrl.pathname;
   const envVariable = context.env.TEST_KEY;
 
-  if (route === '/without-cacheTtl') {
-    // const res = await fetch(`https://random-data-api.com/api/v2/appliances`);
-    const newUrl = new URL('/users', parsedUrl);
-
-    const modifiedRequest = new Request(newUrl, req)
-    const res = await fetch(modifiedRequest);
-    let response = await res.json();
-    response = {
-      ...response,
-      envVariableValue: envVariable,
-    }
-    return new Response(JSON.stringify(response))
-  }
-
-  if (route === '/with-cacheTtl') {
+  if (route === '/test') {
     const modifiedRequest = new Request(new URL('/users', parsedUrl), req)
     const res = await fetch(modifiedRequest, {
       cf:{
