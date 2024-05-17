5af5ccf8fcc0d9f86db8206deb7f541873d7d20c
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-05-17 11:51:37 +0530

    log headers

--- functions/users.js
+++ functions/users.js
@@ -7,6 +7,7 @@ export default function handler(request, response) {
   ];
 
   console.log("query params", request.query)
+  console.log("headers", request.headers)
 
   // response.setHeader('Cache-Control', 'max-age=120');
 
